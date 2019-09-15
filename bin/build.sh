f5e7720b77a5f5fecd81015857fdb195fedb3ed3
Steve Krulewitz <steve.krulewitz@airbnb.com>
2019-09-14 17:25:52 -0700

    many changes

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,17 @@
 mkdir -p ~/Library/texmf/tex/latex
 ln -s "`realpath tex`" ~/Library/texmf/tex/latex/
 
+input_file="`realpath "$1"`"
+
+output_file=$(\
+  cat "$input_file" | \
+  grep "\\\begin{song}" | \
+  sed -E 's/.*title={([^}]*)}, band={([^}]*)}, year={([^}]*)}.*/\1 - \2 (\3)/' | \
+  tr -d '\n' \
+)
+
 pdflatex \
     -interaction=nonstopmode \
     -output-directory pdf \
-    "`realpath "$1"`"
+    -jobname="$output_file" \
+    "$input_file"
