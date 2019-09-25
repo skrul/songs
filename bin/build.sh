21ef5de8df588a8d0298ad2dde3053bd00bbfe26
Steve Krulewitz <steve.krulewitz@airbnb.com>
2019-09-24 22:16:36 -0700

    more songs

--- bin/build.sh
+++ bin/build.sh
@@ -44,4 +44,5 @@ function make_packet {
 setup_pdflatex
 #make_pdf "$1"
 
-make_packet "packets/aircoustic201910.tex"
+#make_packet "packets/aircoustic201910.tex"
+make_packet "packets/pha201910.tex"
